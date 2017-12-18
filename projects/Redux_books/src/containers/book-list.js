7d249187c9944010b3b7c59c16f569db991fc8a3
Nalipp <nateplipp@gmail.com>
2017-12-18 11:42:26 -0600

    actions

--- projects/Redux_books/src/containers/book-list.js
+++ projects/Redux_books/src/containers/book-list.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';  
+// ensures that the actions that are being created flow through all of the reducers (selectBook)
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li 
+          key={book.title} 
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       )
     });
   }
@@ -25,4 +33,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {  
+  // anything returned from this function will end up as props on the book list container
+  return bindActionCreators({selectBook: selectBook}, dispatch); 
+  // whenever selectbook is called the result is passed to all of the reducers (dispatch)
+}  
+// allows you to call this.props.selectBook
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
+// promote BookList from a component to a continer, also allows access to selectBook prop
